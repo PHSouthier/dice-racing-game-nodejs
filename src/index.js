4ed82d613486e053e493fe481028e7e5e63945b1
Pedro_Southier <phsouthier@hotmail.com>
2026-03-17 22:35:35 -0300

    criado funcao para sortear bloco

--- src/index.js
+++ src/index.js
@@ -17,3 +17,21 @@ const player2 = {
 async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
+
+async function getRandomBlock() {
+  let random = Math.random();
+  let result;
+
+  switch (true) {
+    case random < 0.33:
+      result = "RETA";
+      break;
+    case random < 0.66:
+      result = "CURVA";
+      break;
+    default:
+      result = "CONFRONTO";
+  }
+
+  return result;
+}
